758d4fa739bc2e640444d64877632c56a6e8bc93
tmessall <tmessall17@gmail.com>
2020-09-30 10:21:30 -0600

    Added a beginQuiz function on start click, made filler questions for now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
 var intro = document.getElementById("intro");
 var startBtn = document.getElementById("startBtn")
 
+var qOne = {
+    question: "What are noodles",
+    answers: "macaroni",
+    correctAnswer: "macaroni"
+}
+
+var qTwo = {
+    question: "What is the order of letters",
+    answers: "alphabet",
+    correctAnswer: "alphabet",
+}
+
+var questionArr = [qOne, qTwo]
+
+function beginQuiz(e) {
+    e.preventDefault();
+    // Hides the starting information, brings up a question
+    intro.style.display = "none";
+    for (var i = 0; i < questionArr.length; i++) {
+        console.log(questionArr[i].answers);
+    }
+}
+
 // Changes color of the START button on hover
 intro.addEventListener("mouseover", function(e) {
     e.preventDefault();
@@ -12,3 +35,5 @@ intro.addEventListener("mouseover", function(e) {
         startBtn.style.color = "lightsteelblue";
     }
 })
+
+startBtn.addEventListener("click", beginQuiz);
